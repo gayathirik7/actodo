2a4bb9759bb5f757c03005183def31a5ba6784f0
gayathirik7 <gayathirik72004@gmail.com>
2024-10-28 13:26:57 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 const Header=(props)=>{
   return(
     <div>
-        <h1 className="text-3xl font-medium">{props.username}</h1>
+        <h1 className="text-3xl font-medium">Hello {props.username}!!!</h1>
         <p>I help You manage your activities :)</p>
       </div>
   )
